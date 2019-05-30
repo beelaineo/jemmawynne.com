1db6387b6405653d69b3a90b6ebcfd434b75fae3
Joseph Thomas <joseph@good-idea.studio>
2019-05-29 22:28:53 -0700

    feat(sanity): add Product schema

--- sanity/schemas/documents/Product.js
+++ sanity/schemas/documents/Product.js
@@ -1,3 +1,17 @@
+import React from 'react'
+
+const ImagePreview = (props) => {
+	console.log(props)
+	return (
+		<>
+			{props.value.edges.map((imageEdge) => (
+				<img src={imageEdge.node.transformedSrc} alt={imageEdge.node.altText} />
+			))}
+		</>
+	)
+	return null
+}
+
 const Product = {
 	title: 'Products',
 	name: 'shopifyProduct',
@@ -7,30 +21,99 @@ const Product = {
 			title: 'Title',
 			name: 'title',
 			readOnly: true,
-			type: 'string'
+			type: 'string',
+			hidden: true,
 		},
 		{
-			title: 'Banner',
-			name: 'hero',
+			title: 'Page URI',
+			name: 'handle',
+			type: 'string',
+			readOnly: true,
+			hidden: true,
+		},
+		{
+			title: 'Shopify ID',
+			name: 'shopifyId',
+			type: 'string',
+			hidden: true,
+		},
+		{
+			title: 'Shopify Data',
+			name: 'sourceData',
+			readOnly: true,
 			type: 'object',
 			fields: [
+				{ title: 'Title', name: 'title', type: 'string' },
+				{
+					title: 'Handle',
+					name: 'handle',
+					type: 'string',
+				},
+				{
+					title: 'Description',
+					name: 'description',
+					type: 'text',
+					rows: 3,
+				},
+				{
+					title: 'ID',
+					name: 'id',
+					type: 'string',
+					hidden: true,
+				},
 				{
 					title: 'Images',
 					name: 'images',
-					type: 'array',
-					of: [
+					type: 'object',
+					inputComponent: ImagePreview,
+					fields: [
 						{
-							type: 'image',
-							options: {
-								hotspot: true,
-							},
+							title: 'edges',
+							name: 'edges',
+							type: 'array',
+							of: [
+								{
+									name: 'node',
+									type: 'object',
+									fields: [
+										{
+											title: 'altText',
+											name: 'altText',
+											type: 'string',
+										},
+										{
+											title: 'id',
+											name: 'id',
+											type: 'string',
+										},
+										{
+											title: 'originalSrc',
+											name: 'originalSrc',
+											type: 'string',
+										},
+										{
+											title: 'w100',
+											name: 'w100',
+											type: 'string',
+										},
+										{
+											title: 'w300',
+											name: 'w300',
+											type: 'string',
+										},
+										{
+											title: 'w800',
+											name: 'w800',
+											type: 'string',
+										},
+									],
+								},
+							],
 						},
 					],
-					validation: Rule => Rule.required().max(2),
 				},
 			],
 		},
-
 	],
 	preview: {
 		select: {
