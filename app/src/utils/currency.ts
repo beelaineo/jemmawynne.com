6310118d8b368572046141a6751d0584abf1c911
misteroh <madeline@done-digital.com>
2019-07-09 15:14:38 -0700

    fix(app): update PLP according to styles requested

---
+++ app/src/utils/currency.ts
@@ -0,0 +1,5 @@
+export const formatMoney = (variantPrice, country) => {
+	// let price = new Intl.NumberFormat('en-IN', { style: 'currency', currency: country }).format(variantPrice));
+	let price = variantPrice
+	return price
+}
