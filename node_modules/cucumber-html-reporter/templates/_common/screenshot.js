0586c6eb0b0ab5f9de4490992b7ed68665e7af11
misteroh <madeline@done-digital.com>
2019-07-31 15:23:52 -0700

    fix(app): pushing to save before new code

--- node_modules/cucumber-html-reporter/templates/_common/screenshot.js
+++
@@ -1,9 +0,0 @@
-$('a.toggle').on('click', function() {
-    if ($(this).text() === 'Screenshot -') {
-        $(this).text('Screenshot +');
-        $(this).next('a.screenshot').find('img').hide();
-    } else {
-        $(this).text('Screenshot -');
-        $(this).next('a.screenshot').find('img').show();
-    }
-});
--- node_modules/react-testing-library/dist/__tests__/no-act.js
+++
@@ -1,9 +0,0 @@
-import {act} from '..'
-
-jest.mock('react-dom/test-utils', () => ({}))
-
-test('act works even when there is no act from test utils', () => {
-  const callback = jest.fn()
-  act(callback)
-  expect(callback).toHaveBeenCalledTimes(1)
-})
