9c9dc635d079185c40f24b152b2b33fa833aeb21
Joseph Thomas <joseph@good-idea.studio>
2019-07-31 15:39:12 -0700

    feat(sanity): add productInfo blocks

--- sanity/schemas/documents/Product.js
+++ sanity/schemas/documents/Product.js
@@ -1,3 +1,14 @@
 import { createProductDocument } from '@sane-shopify/sanity-plugin'
 
-export const Product = createProductDocument()
+export const Product = createProductDocument({
+	fields: [
+		{
+			title: 'Info Blocks',
+			name: 'infoBlocks',
+			type: 'array',
+			description:
+				'Info blocks here will appear as accordion-dropdowns, below the product description. You can also add content blocks to multiple items in the "Product Info" section of the CMS.',
+			of: [{ type: 'productInfoBlock' }],
+		},
+	],
+})
