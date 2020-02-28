5a19e15dc48281649e404b101d3067261a11dcb7
Joseph Thomas <joseph@good-idea.studio>
2020-02-27 19:27:24 -0800

    chore(app): set up babel-plugin-styled-components

--- app/.babelrc.js
+++ app/.babelrc.js
@@ -1,3 +1,19 @@
 module.exports = {
   presets: ['next/babel'],
+  plugins: [
+    [
+      'babel-plugin-styled-components',
+      { ssr: true, displayName: true, preprocess: false },
+    ],
+  ],
+  env: {
+    production: {
+      plugins: [
+        [
+          'babel-plugin-styled-components',
+          { ssr: true, displayName: false, preprocess: false },
+        ],
+      ],
+    },
+  },
 }
