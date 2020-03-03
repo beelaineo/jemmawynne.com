f422547b715879e73110fe080b4584f3ddb6b133
Joseph Thomas <joseph@good-idea.studio>
2020-03-03 15:02:29 -0800

    feat(sanity): add static page content

--- sanity/schemas/documents/page.js
+++ sanity/schemas/documents/page.js
@@ -1,22 +1,65 @@
 export const page = {
-	title: 'Page',
-	type: 'document',
-	name: 'page',
-	fields: [
-		{
-			name: 'title',
-			label: 'Title',
-			type: 'string',
-			validation: (Rule) => Rule.required(),
-		},
-		{
-			name: 'slug',
-			label: 'Page URL',
-			type: 'slug',
-			options: {
-				source: 'title',
-			},
-			validation: (Rule) => Rule.required(),
-		},
-	],
+  title: 'Page',
+  type: 'document',
+  name: 'page',
+  fields: [
+    {
+      name: 'title',
+      label: 'Title',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'slug',
+      label: 'Page URL',
+      type: 'slug',
+      options: {
+        source: 'title',
+      },
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'Hero',
+      type: 'hero',
+    },
+    {
+      name: 'textAlign',
+      title: 'Content Text Alignment',
+      type: 'textAlign',
+    },
+    {
+      name: 'content',
+      label: 'Content',
+      type: 'array',
+      of: [
+        {
+          type: 'block',
+          styles: [
+            { title: 'Header 2', value: 'h2' },
+            { title: 'Header 3', value: 'h3' },
+            { title: 'Normal', value: 'normal' },
+          ],
+          marks: {
+            decorators: [
+              { title: 'Strong', value: 'strong' },
+              { title: 'Emphasis', value: 'em' },
+            ],
+          },
+          lists: [
+            {
+              title: 'Bullet',
+              value: 'bullet',
+            },
+            {
+              title: 'Numbered',
+              value: 'number',
+            },
+          ],
+        },
+        {
+          type: 'richImage',
+        },
+      ],
+    },
+  ],
 }
