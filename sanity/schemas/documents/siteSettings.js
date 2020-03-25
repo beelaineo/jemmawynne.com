029f5705e9d44a53cfc6988a692818be028ae4cd
Joseph Thomas <joseph@good-idea.studio>
2020-03-25 10:15:46 -0700

    feat(cms): add announcement

--- sanity/schemas/documents/siteSettings.js
+++ sanity/schemas/documents/siteSettings.js
@@ -1,9 +1,32 @@
+export const banner = {
+  name: 'banner',
+  title: 'Announcement Banner',
+  type: 'object',
+  fields: [
+    {
+      name: 'text',
+      title: 'Text',
+      type: 'string',
+    },
+    {
+      name: 'cta',
+      title: 'CTA',
+      type: 'cta',
+    },
+  ],
+}
+
 export const siteSettings = {
   name: 'siteSettings',
   title: 'Site Settings',
   type: 'document',
   fieldsets: [{ name: 'footer', title: 'Footer' }],
   fields: [
+    {
+      name: 'banner',
+      title: 'Announcement Banner',
+      type: 'banner',
+    },
     {
       name: 'links',
       fieldset: 'footer',
