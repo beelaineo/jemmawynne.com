f628137a0a32706a8dedbcfa8a63b638bccf016b
Joseph Thomas <joseph@good-idea.studio>
2020-06-15 12:27:59 -0700

    chore(sanity): update dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,7 @@ module.exports = {
     '@typescript-eslint/explicit-member-accessibility': 'off',
     '@typescript-eslint/ban-ts-ignore': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/camelcase': 'off',
   },
   settings: {
     react: {
