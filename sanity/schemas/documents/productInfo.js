8a42646f31e8e52808f8d97e9c6317471e57c243
Joseph Thomas <joseph@good-idea.studio>
2020-07-06 15:20:10 -0700

    feat(sanity): add tag badge configuration

--- sanity/schemas/documents/productInfo.js
+++ sanity/schemas/documents/productInfo.js
@@ -1,3 +1,33 @@
+export const tagBadge = {
+  name: 'tagBadge',
+  title: 'Tag Badge',
+  type: 'object',
+  fields: [
+    {
+      name: 'tag',
+      type: 'string',
+      title: 'Tag',
+      description: 'The tag to match from Shopify',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'label',
+      type: 'string',
+      title: 'Label',
+      description: '(optional) An alternate label to display in the badge',
+    },
+  ],
+  preview: {
+    select: {
+      tag: 'tag',
+      label: 'label',
+    },
+    prepare: ({ tag, label }) => ({
+      title: label || tag,
+    }),
+  },
+}
+
 export const productInfo = {
   title: 'Product Page Settings',
   type: 'document',
@@ -5,27 +35,58 @@ export const productInfo = {
   description: 'description',
   fieldsets: [
     {
-      name: 'byType',
+      name: 'tagBadges',
+      title: 'Tag Badges',
+    },
+    {
+      name: 'globalAccordions',
       title: 'By Type',
     },
     {
+      name: 'byType',
+      title: 'Accordions By Type',
+    },
+    {
       name: 'byTag',
-      title: 'By Tag',
+      title: 'Accordions By Tag',
     },
   ],
   fields: [
     {
+      name: 'tagBadgeHelpText',
+      type: 'helpText',
+      fieldset: 'tagBadges',
+      description:
+        'Use these fields to create badges that appear on product thumbnails',
+    },
+    {
+      name: 'tagBadges',
+      type: 'array',
+      fieldset: 'tagBadges',
+      of: [{ type: 'tagBadge' }],
+    },
+
+    {
       name: 'helpText',
       type: 'helpText',
+      fieldset: 'globalAccordions',
       description:
         "Use these fields to add snippets of descriptions to all or some projects. For instance, you could add a 'Shipping and Returns' block on all items, and a 'Ring Sizing Guide' block to all Rings. These blocks will be displayed in accordion-dropdowns below the main product information. You can also add info blocks to individual items on their page here in the CMS.",
     },
     {
       name: 'globalBlocks',
       label: 'Global Blocks',
+      fieldset: 'globalAccordions',
       description: 'These blocks will appear on every product',
       type: 'array',
       of: [{ type: 'productInfoBlock' }],
+    },
+    {
+      name: 'byTypeHelpText',
+      type: 'helpText',
+      fieldset: 'byType',
+      description:
+        'Use these fields to add snippets of descriptions to all products of different types',
     },
     {
       name: 'ringBlocks',
