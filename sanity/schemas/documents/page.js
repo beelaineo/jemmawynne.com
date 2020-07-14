d74098dc65f46e897e6a14f6c05e34665151325a
Joseph Thomas <joseph@good-idea.studio>
2020-07-14 10:06:49 -0700

    feat(sanity): add new about page to schema

--- sanity/schemas/documents/page.js
+++ sanity/schemas/documents/page.js
@@ -1,3 +1,65 @@
+import * as React from 'react'
+import { BlockPreview } from '../components/BlockPreview'
+import { getImageThumbnail, blocksToPlainText } from '../utils'
+
+const getPageBlockPreviewValues = async ({ content }) => {
+  const firstImage = content.find((c) => c._type === 'richImage')
+  const firstText = content.find((c) => c._type === 'pageText')
+  const src = firstImage ? await getImageThumbnail(firstImage) : undefined
+  const title = firstText ? firstText.heading : '(no text)'
+  const subtitles = firstText ? [blocksToPlainText(firstText.body)] : undefined
+  return { title, subtitles, src }
+}
+
+export const pageText = {
+  title: 'Page Text',
+  name: 'pageText',
+  type: 'object',
+  fields: [
+    {
+      name: 'heading',
+      type: 'string',
+      title: 'Heading',
+    },
+    {
+      name: 'body',
+      type: 'richText',
+    },
+  ],
+}
+
+export const pageBlock = {
+  title: 'Page Block',
+  name: 'pageBlock',
+  type: 'object',
+  fields: [
+    {
+      name: 'backgroundColor',
+      type: 'colorPicker',
+      title: 'Background Color',
+    },
+    {
+      name: 'alignment',
+      type: 'position',
+      title: 'Content Alignment',
+    },
+    {
+      name: 'content',
+      type: 'array',
+      of: [{ type: 'pageText', isHighlighted: true }, { type: 'richImage' }],
+      validation: (Rule) => Rule.max(3),
+    },
+  ],
+  preview: {
+    select: {
+      content: 'content',
+    },
+    component: (props) => (
+      <BlockPreview {...props} getPreviewValues={getPageBlockPreviewValues} />
+    ),
+  },
+}
+
 export const page = {
   title: 'Page',
   type: 'document',
@@ -24,9 +86,10 @@ export const page = {
       type: 'hero',
     },
     {
-      name: 'textAlign',
-      title: 'Content Text Alignment',
-      type: 'textAlign',
+      name: 'body',
+      label: 'Content',
+      type: 'array',
+      of: [{ type: 'pageBlock' }, { type: 'hero' }, { type: 'carousel' }],
     },
     {
       name: 'content',
