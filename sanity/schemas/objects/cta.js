f22b099a109c04a696b8487c76b800391676b6cd
Joseph Thomas <joseph@good-idea.studio>
2021-02-22 09:56:00 -0800

    feat(cms): add mailto link to CTA

--- sanity/schemas/objects/cta.js
+++ sanity/schemas/objects/cta.js
@@ -5,6 +5,20 @@ export const cta = {
   title: 'CTA Button',
   type: 'object',
   icon: GiClick,
+  fieldsets: [
+    {
+      name: 'internalLink',
+      title: 'Internal Link',
+      options: { collapsible: true, collapsed: false },
+    },
+    {
+      name: 'mailToLink',
+      title: 'Email link',
+      description:
+        "Use these fields to make the button open a new email in the user's email client. This will only work if the,",
+      option: { collapsible: true, collapsed: true },
+    },
+  ],
   fields: [
     {
       name: 'label',
@@ -12,11 +26,34 @@ export const cta = {
       label: 'Label',
     },
     {
-      type: 'internalLink',
       name: 'link',
-      options: {
-        required: true,
-      },
+      type: 'internalLink',
+    },
+    {
+      name: 'mailToEmail',
+      type: 'email',
+      fieldset: 'mailToLink',
+      title: 'To: email address',
+      validation: (Rule) =>
+        Rule.custom((mailToEmail, ctx) => {
+          if (mailToEmail.length && ctx.parent?.link?.document) {
+            return 'You must remove the linked document for this CTA to enable an email link'
+          }
+          return true
+        }).optional(),
+    },
+    {
+      name: 'mailToSubject',
+      type: 'string',
+      fieldset: 'mailToLink',
+      title: 'Email Subject',
+      validation: (Rule) =>
+        Rule.custom((mailToSubject, ctx) => {
+          if (mailToSubject.length && ctx.parent?.link?.document) {
+            return 'You must remove the linked document for this CTA to enable an email link'
+          }
+          return true
+        }).optional(),
     },
   ],
 }
