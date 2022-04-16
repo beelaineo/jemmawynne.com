e706d1fc443b57e47032a00ca6a4579ac209a5c9
Joseph <joseph@good-idea.studio>
2022-04-16 11:03:35 -0700

    fix(app): fix hard-coded graphql endpoint

--- app/src/config/index.ts
+++ app/src/config/index.ts
@@ -1,6 +1,8 @@
+const SANITY_PROJECT_ID = process.env.SANITY_PROJECT_ID
+const SANITY_DATASET = process.env.SANITY_DATASET
+
 export const SHOPIFY_STOREFRONT_TOKEN = '969193ad0bbcaf8485a301bffa9d1058'
 export const SHOPIFY_STOREFRONT_URL =
   'https://jemma-wynne.myshopify.com/api/graphql'
-export const SANITY_GRAPHQL_URL =
-  'https://caazz4uw.api.sanity.io/v1/graphql/staging/default'
+export const SANITY_GRAPHQL_URL = `https://${SANITY_PROJECT_ID}.api.sanity.io/v1/graphql/${SANITY_DATASET}/default`
 export const FB_PRDOUCT_CATALOG_ID = 'TODO'
