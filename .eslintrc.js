c1471dcfb61d5152a9c4bf3aad74d320f23bcfcf
Blaine <blaine.d.oneill@gmail.com>
2022-06-20 12:53:50 +0200

    fix(sanity): fix eslint settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,16 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
     'prettier',
+    'plugin:prettier/recommended',
+    'plugin:import/typescript',
+    'plugin:import/recommended',
   ],
   parserOptions: {
     sourceType: 'module',
   },
   rules: {
     'no-unused-vars': 'off',
+    'import/no-unresolved': [2, { ignore: ['^(all|part):'] }],
     'react/prop-types': 'off',
     'react/display-name': 'off',
     '@typescript-eslint/no-empty-interface': 'warn',
